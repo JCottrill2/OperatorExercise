058cba75d7706e6ca401de95c86ef5e4b36b3fa2
jess <jcottrill1123@gmail.com>
2022-08-10 11:26:18 -0400

    initial commit

---
+++ Operations/Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations
+{
+    internal class Methods
+    {
+        public static double AreaofCircle(double radius)
+        {
+            double answer = Math.PI * Math.Pow(radius, 2);
+            return answer;
+           
+        }
+
+
+    }
+}
---
+++ Operations/Program.cs
@@ -0,0 +1,29 @@
+﻿using Operations;
+
+int num1 = 15;
+int num2 = 5;
+
+Console.WriteLine($"Add: {num1+num2}");
+Console.WriteLine($"Subtract: {num1+num2}");
+Console.WriteLine($"Multiply: {num1+num2}");
+
+int a = 17;
+int b = 4;
+
+int quotient = a / b;
+int remainder = a % b;
+
+if (a == 17 && b == 4)
+{
+Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder} ");
+}
+
+
+
+
+Console.WriteLine("Please put in the radius of circle");
+var userInput = double.Parse(Console.ReadLine());
+
+double area = Methods.AreaofCircle(userInput);
+
+Console.WriteLine(area);
